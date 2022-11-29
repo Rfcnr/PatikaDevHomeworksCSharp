32a68bcbdff6d8333e1fc26741db2007435c0ba5
Raif Cinar <raifcinar81@gmail.com>
2022-11-30 02:00:57 +0300

    added -  algorithm questions homework 1 part 2

---
+++ CSharp101/Homework1/AlgorithmQuestions2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmQuestions2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Homework:
+                In a console application, ask the user to enter two positive numbers (n, m).
+                Then ask the user to enter n positive numbers. From the numbers entered by the user.
+                Print the numbers that are divisible by m to the console.
+
+                Note: No exception checks have been made.
+            */
+
+            int n, m;
+            Console.WriteLine("Write how many numbers you want to enter: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the multiplier: ");
+            m = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Please enter the number: ");
+
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("****************\nThe numbers you enter are divisible by the multiplier (" + m + ")");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % m == 0) Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
