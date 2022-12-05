8043c447a39c5a01c3e440ed4ecc01f4ad85db90
Raif Cinar <raifcinar81@gmail.com>
2022-12-05 17:02:00 +0300

    added - simple phone book app

---
+++ CSharp101/SimplePhoneBookApp/IPhoneBook.cs
@@ -0,0 +1,19 @@
+﻿namespace SimplePhoneBookApp
+{
+    public interface IPhoneBook
+    {
+        void AddNewContact();
+
+        void DeleteContact();
+
+        void UpdateContact();
+
+        void ListAllContacts();
+
+        void SearchContact();
+
+        void PreferencesOrExitMenu();
+
+        void ExitPhoneBook();
+    }
+}
